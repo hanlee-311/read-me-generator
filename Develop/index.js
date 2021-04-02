ba07a46eefe5c233ccce68553705180cfe0e463f
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-01 20:33:34 -0500

    downloaded inquirer npm, added questions in the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
+// Completed: Downloaded Inquierer
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", "Please write a description of your project.", "What are the installation instructions?", "What is the usage information?", "What are the contribution guidelines?", "What are the test instructions?", "Please choose a license for your application.", "Enter your Github username.", "What is your email address?", "Please explain how someone can reach you."];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

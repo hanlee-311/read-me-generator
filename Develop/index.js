e6b0d603a708496e625e08c4e4f847a678b61242
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-01 21:01:39 -0500

    added questions for the user to answer. Input is saved and generated into a txt file at this time

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 // Completed: Downloaded Inquierer
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of your project?", "Please write a description of your project.", "What are the installation instructions?", "What is the usage information?", "What are the contribution guidelines?", "What are the test instructions?", "Please choose a license for your application.", "Enter your Github username.", "What is your email address?", "Please explain how someone can reach you."];
@@ -8,7 +11,58 @@ const questions = ["What is the title of your project?", "Please write a descrip
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: questions[0],
+        name: 'Name',
+        },
+        {
+         type: 'input',
+         message: questions[1],
+         name: 'Description',   
+        },
+        {
+        type: 'input',
+        message: questions[2],
+        name: 'Usage-Info',
+        },
+        {
+        type: 'input',
+        message: questions[3],
+        name: 'Contribtuion-Info',
+        },
+        {
+        type: 'input',
+        message: questions[4],
+        name: 'Test-Info',
+        },
+        {
+        type: 'input',
+        message: questions[6],
+        name: 'Github-username',
+        },
+        {
+        type: 'input',
+        message: questions[7],
+        name: 'Email',
+        },
+        {
+        type: 'input',
+        message: questions[8],
+        name: 'Contact-Info',
+        }
+    ])
+    .then((response) =>
+        {console.log(response)
+        let answers = JSON.stringify(response);
+        fs.writeFile('log.txt', answers, (err) =>
+        err ? console.log(err) : console.log('Success!')
+        )
+    });
+}
 
 // Function call to initialize app
 init();
+
